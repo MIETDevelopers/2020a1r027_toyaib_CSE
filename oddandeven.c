9706c2a2344c16a84beb51d50599e666aa1b3481
unknown <toyaibmalik67@gmail.com>
2021-03-31 23:18:36 +0530

    odd and even

---
+++ oddandeven.c
@@ -0,0 +1,15 @@
+//Program to check whether no. is odd or even.
+#include <stdio.h>//header file
+int main()//main function 
+{
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);//scanning number
+    //checking condition
+    if(num % 2 == 0)
+        printf("%d is even.", num);//number is even
+    else
+        printf("%d is odd.", num);//number is odd
+
+    return 0;
+}
